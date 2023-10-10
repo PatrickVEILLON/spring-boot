bdbe913a8257b581d8eea846f474db89bfb6e738
nuisser-dev <117860493+nuisser-dev@users.noreply.github.com>
2023-10-10 21:37:06 +0100

    Add files via upload

---
+++ produit/src/main/java/com/amine/demo/ProduitApplication.java
@@ -0,0 +1,13 @@
+package com.amine.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduitApplication.class, args);
+	}
+
+}
